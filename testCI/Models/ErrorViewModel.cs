972a6009fdbf90db35d89084ea5a25a52ba0e582
Kaiwanyawit Chawankul <kaiwanyawit.c@draycir.com>
2019-12-08 20:16:51 +0700

    add basedline

---
+++ testCI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testCI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
