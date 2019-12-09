bcfa08c3371a442e4a00661fa5d2ec7add0f65c3
Kaiwanyawit Chawankul <Kaiwanyawit.C@draycir.com>
2019-12-09 09:21:45 +0700

    update tests

--- testCI.Tests/UnitTest1.cs
+++ testCI.Tests/UnitTest1.cs
@@ -1,15 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using testCI.Controllers;
 
 namespace testCI.Tests
 {
     [TestClass]
     public class UnitTest1
     {
+        private HomeController _target;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _target = new HomeController();
+        }
         [TestMethod]
         public void TestMethod1()
         {
             //Assert.Fail("No tests");
             Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Index_Test()
+        {
+            var result = _target.Index();
+            // Assert
+            var viewResult = result as ViewResult;
+            //var model = Assert.IsAssignableFrom<IEnumerable<StormSessionViewModel>>(
+            //    viewResult.ViewData.Model);
+            Assert.IsNotNull(viewResult);
         }
     }
 }
